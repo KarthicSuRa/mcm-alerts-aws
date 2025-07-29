3540f9ef141ef267635547ca48ed3a9983a3c02a
KarthicSuRa <153284275+KarthicSuRa@users.noreply.github.com>
2025-07-29 17:17:41 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  define: {
+    global: 'globalThis',
   },
 })
